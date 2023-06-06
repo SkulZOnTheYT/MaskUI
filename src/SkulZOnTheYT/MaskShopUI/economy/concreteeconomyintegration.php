5ef1179eae3f402554c1b1406d875ff3747fffca
SkulZOnTheYT <92020118+SkulZOnTheYT@users.noreply.github.com>
2023-06-06 20:31:42 +0700

    Update concreteeconomyintegration.php

--- src/SkulZOnTheYT/MaskShopUI/economy/concreteeconomyintegration.php
+++ src/SkulZOnTheYT/MaskShopUI/economy/concreteeconomyintegration.php
@@ -9,24 +9,10 @@ use SkulZOnTheYT\MaskShopUI\economy\EconomyIntegration;
 
 class ConcreteEconomyIntegration implements EconomyIntegration {
   
-	public function getMoney(Player $player, Closure $callback) : void;
+	public function getMoney() : void;
 
-	/**
-	 * Adds a given amount of money to the player.
-	 *
-	 * @param Player $player
-	 * @param float $money
-	 */
-	public function addMoney(Player $player, float $money) : void;
+	public function addMoney() : void;
 
-	/**
-	 * Removes a given amount of money from the player.
-	 *
-	 * @param Player $player
-	 * @param Closure $callback
-	 *
-	 * @phpstan-param Closure(bool) : void $callback
-	 */
-	public function removeMoney(Player $player, float $money, Closure $callback) : void;
+	public function removeMoney() : void;
 
 }

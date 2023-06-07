f59de4551e8815c9caf81827b9a489b352ef3894
SkulZOnTheYT <92020118+SkulZOnTheYT@users.noreply.github.com>
2023-06-07 08:43:29 +0700

    Update BedrockEconomyIntegration.php

--- src/SkulZOnTheYT/MaskShopUI/economy/BedrockEconomyIntegration.php
+++ src/SkulZOnTheYT/MaskShopUI/economy/BedrockEconomyIntegration.php
@@ -16,34 +16,22 @@ use function assert;
 
 final class BedrockEconomyIntegration implements EconomyIntegration{
 
-	private $plugin;
-	
-	public function __construct(){
-		/** @var BedrockEconomy|null $plugin */
-		$plugin = Server::getInstance()->getPluginManager()->getPlugin("BedrockEconomy");
-		if($plugin === null){
-			throw new InvalidArgumentException("BedrockEconomy plugin was not found");
-		}
-
-		$this->plugin = $plugin;
-	}
-
     public function init(array $config) : void{
 	}
 
 	public function getMoney(Player $player, Closure $callback) : void{
-		BedrockEconomyAPI::getInstance()->getPlayerBalance($player->getName(), ClosureContext::create(static function(?int $balance) use($callback) : void{
+		$player->getInstance()->getPlayerBalance($player->getName(), ClosureContext::create(static function(?int $balance) use($callback) : void{
 			$callback($balance ?? 0);
 		}));
 	}
 
 	public function addMoney(Player $player, float $money) : void{
-		BedrockEconomyAPI::getInstance()->addToPlayerBalance($player->getName(), (int) ceil($money));
+		$player->getInstance()->addToPlayerBalance($player->getName(), (int) ceil($money));
 	}
 
 	//Thanks for the Repair @cooldogedev
 	public function removeMoney(Player $player, float $money, Closure $callback) : void{
-		BedrockEconomyAPI::getInstance()->subtractFromPlayerBalance($player->getName(), (int) ceil($money), ClosureContext::create(static function(bool $success) use($callback) : void{
+		$player->getInstance()->subtractFromPlayerBalance($player->getName(), (int) ceil($money), ClosureContext::create(static function(bool $success) use($callback) : void{
 			$callback($success);
 		}));
 	}

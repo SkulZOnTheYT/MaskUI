4f28ffa1857fb49a9ab0ce24c3ee50d7b14f750f
SkulZOnTheYT <92020118+SkulZOnTheYT@users.noreply.github.com>
2023-06-09 22:05:35 +0700

    Update EffectTask.php

--- src/SkulZOnTheYT/MaskUI/EffectTask.php
+++ src/SkulZOnTheYT/MaskUI/EffectTask.php
@@ -16,39 +16,33 @@ use pocketmine\utils\TextFormat;
 use SkulZOnTheYT\MaskUI\Main;
 
 class EffectTask extends Task {
-
-    private $player;
     
-    public const DRAGON_HEAD = Item::DRAGON_HEAD;
-    public const CREEPER_HEAD = Item::CREEPER_HEAD;
-    public const WITHER_SKELETON_SKULL = Item::WITHER_SKELETON_SKULL;
-    public const STEVE_HEAD = Item::PLAYER_HEAD;
-    public const SKELETON_SKULL = Item::SKELETON_SKULL;
-    public const ZOMBIE_HEAD = Item::ZOMBIE_HEAD;
-    
-    public function __construct(Player $player) {
-        $this->player = $player;
-    }
+    $dragon = ItemFactory::getInstance()->get(397, 5, 1);
+    $creeper = ItemFactory::getInstance()->get(397, 4, 1);
+    $wither = ItemFactory::getInstance()->get(397, 1, 1);
+    $steve = ItemFactory::getInstance()->get(397, 3, 1);
+    $skeleton = ItemFactory::getInstance()->get(397, 0, 1);
+    $zombie = ItemFactory::getInstance()->get(397, 2, 1);
 
-    public function onRun(int $currentTick) {
+    public function onRun(int $currentTick) void {
         $helmet = $this->player->getArmorInventory()->getHelmet();
 
         if ($helmet !== null) {
             switch ($helmet->getId()) {
-                case self::DRAGON_HEAD:
+                case $dragon:
                     $this->applyDragonHeadEffects();
                     break;
-                case self::CREEPER_HEAD:
+                case $creeper:
                     $this->applyCreeperHeadEffects();
                     break;
-                case self::SKELETON_SKULL:
-                case self::WITHER_SKELETON_SKULL:
+                case $wither:
+                case $skeleton:
                     $this->applySkeletonHeadEffects();
                     break;
-                case self::STEVE_HEAD:
+                case $steve:
                     $this->applySteveHeadEffects();
                     break;
-                case self::ZOMBIE_HEAD:
+                case $zombie:
                     $this->applyZombieHeadEffects();
                     break;
             }

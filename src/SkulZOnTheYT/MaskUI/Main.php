1f9b578b63361586bda89ec80fd6512c30d0719b
SkulZOnTheYT <92020118+SkulZOnTheYT@users.noreply.github.com>
2023-06-22 09:17:18 +0700

    Update Main.php

--- src/SkulZOnTheYT/MaskUI/Main.php
+++ src/SkulZOnTheYT/MaskUI/Main.php
@@ -30,10 +30,6 @@ class Main extends PluginBase implements Listener {
 
 	private $player;
 
-	public function __construct (Player $player) {
-		$this->player = $player;
-	}
-
 	public function getPlayer(): Player {
         return $this->player;
 	}

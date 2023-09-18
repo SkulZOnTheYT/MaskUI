a7147b158d34b21fbb99b3f496c2f8bdfa53207c
SkulZOnTheYT <92020118+SkulZOnTheYT@users.noreply.github.com>
2023-09-18 11:11:42 +0700

    nerf and buff effect mask

--- src/SkulZOnTheYT/MaskUI/EffectTask.php
+++ src/SkulZOnTheYT/MaskUI/EffectTask.php
@@ -30,36 +30,36 @@ class EffectTask extends Task {
        foreach ($server->getOnlinePlayers() as $player) {
         $helmet = $player->getArmorInventory()->getHelmet();
             if ($helmet->getStateId() === VanillaBlocks::MOB_HEAD()->setMobHeadType(MobHeadType::SKELETON())->asItem()->getStateId()) {
-	      $player->getEffects()->add(new EffectInstance(VanillaEffects::FIRE_RESISTANCE(), 300, 3, false));
-      	      $player->getEffects()->add(new EffectInstance(VanillaEffects::JUMP_BOOST(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::HEALTH_BOOST(), 300, 4, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::SPEED(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::INSTANT_HEALTH(), 300, 2, false));
+	      $player->getEffects()->add(new EffectInstance(VanillaEffects::STRENGTH(), 300, 0, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 1, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::JUMP_BOOST(), 300, 0, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::REGENERATION(), 300, 0, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::FIRE_RESISTANCE(), 220, 0, false));
+	      $player->getEffects()->add(new EffectInstance(VanillaEffects::HEALTH_BOOST(), 300, 1, false));
            }
 	   if ($helmet->getStateId() === VanillaBlocks::MOB_HEAD()->setMobHeadType(MobHeadType::ZOMBIE())->asItem()->getStateId()) {
-	      $player->getEffects()->add(new EffectInstance(VanillaEffects::FIRE_RESISTANCE(), 300, 3, false));
-      	      $player->getEffects()->add(new EffectInstance(VanillaEffects::JUMP_BOOST(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::HEALTH_BOOST(), 300, 4, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::SPEED(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::INSTANT_HEALTH(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::STRENGTH(), 300, 2, false));
+	      $player->getEffects()->add(new EffectInstance(VanillaEffects::JUMP_BOOST(), 300, 1, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::STRENGTH(), 300, 1, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 1, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::REGENERATION(), 300, 1, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::FIRE_RESISTANCE(), 300, 0, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::SPEED(), 300, 0, false));
+	      $player->getEffects()->add(new EffectInstance(VanillaEffects::HEALTH_BOOST(), 300, 1, false));
 	   } 
 	   if ($helmet->getStateId() === VanillaBlocks::MOB_HEAD()->setMobHeadType(MobHeadType::CREEPER())->asItem()->getStateId()) {
-	      $player->getEffects()->add(new EffectInstance(VanillaEffects::FIRE_RESISTANCE(), 300, 3, false));
-      	      $player->getEffects()->add(new EffectInstance(VanillaEffects::JUMP_BOOST(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::HEALTH_BOOST(), 300, 4, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::SPEED(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::INSTANT_HEALTH(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::STRENGTH(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::SATURATION(), 300, 2, false));
+	      $player->getEffects()->add(new EffectInstance(VanillaEffects::SPEED(), 300, 1, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::STRENGTH(), 300, 1, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::REGENERATION(), 300, 1, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::HEALTH_BOOST(), 300, 1, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::FIRE_RESISTANCE(), 300, 1, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::JUMP_BOOST(), 300, 1, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 1, false));
+	      $player->getEffects()->add(new EffectInstance(VanillaEffects::SATURATION(), 300, 1, false));
 	   }
 	   if ($helmet->getStateId() === VanillaBlocks::MOB_HEAD()->setMobHeadType(MobHeadType::PIGLIN())->asItem()->getStateId()) {
-	      $player->getEffects()->add(new EffectInstance(VanillaEffects::FIRE_RESISTANCE(), 300, 3, false));
+	      $player->getEffects()->add(new EffectInstance(VanillaEffects::FIRE_RESISTANCE(), 300, 2, false));
       	      $player->getEffects()->add(new EffectInstance(VanillaEffects::JUMP_BOOST(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::HEALTH_BOOST(), 300, 4, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::HEALTH_BOOST(), 300, 2, false));
               $player->getEffects()->add(new EffectInstance(VanillaEffects::SPEED(), 300, 2, false));
               $player->getEffects()->add(new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 2, false));
               $player->getEffects()->add(new EffectInstance(VanillaEffects::INSTANT_HEALTH(), 300, 2, false));
@@ -70,41 +70,41 @@ class EffectTask extends Task {
 	   if ($helmet->getStateId() === VanillaBlocks::MOB_HEAD()->setMobHeadType(MobHeadType::PLAYER())->asItem()->getStateId()) {
 	      $player->getEffects()->add(new EffectInstance(VanillaEffects::FIRE_RESISTANCE(), 300, 3, false));
       	      $player->getEffects()->add(new EffectInstance(VanillaEffects::JUMP_BOOST(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::HEALTH_BOOST(), 300, 4, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::HEALTH_BOOST(), 300, 3, false));
               $player->getEffects()->add(new EffectInstance(VanillaEffects::SPEED(), 300, 2, false));
               $player->getEffects()->add(new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::INSTANT_HEALTH(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::STRENGTH(), 300, 2, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::INSTANT_HEALTH(), 300, 3, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::STRENGTH(), 300, 3, false));
               $player->getEffects()->add(new EffectInstance(VanillaEffects::SATURATION(), 300, 2, false));
               $player->getEffects()->add(new EffectInstance(VanillaEffects::REGENERATION(), 300, 2, false));
 	      $player->getEffects()->add(new EffectInstance(VanillaEffects::WATER_BREATHING(), 300, 2, false));
 	   }
 	   if ($helmet->getStateId() === VanillaBlocks::MOB_HEAD()->setMobHeadType(MobHeadType::WITHER_SKELETON())->asItem()->getStateId()) {
 	      $player->getEffects()->add(new EffectInstance(VanillaEffects::FIRE_RESISTANCE(), 300, 3, false));
-      	      $player->getEffects()->add(new EffectInstance(VanillaEffects::JUMP_BOOST(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::HEALTH_BOOST(), 300, 4, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::SPEED(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::INSTANT_HEALTH(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::STRENGTH(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::SATURATION(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::REGENERATION(), 300, 2, false));
-	      $player->getEffects()->add(new EffectInstance(VanillaEffects::WATER_BREATHING(), 300, 2, false));
-	      $player->getEffects()->add(new EffectInstance(VanillaEffects::HASTE(), 300, 2, false));
+      	      $player->getEffects()->add(new EffectInstance(VanillaEffects::JUMP_BOOST(), 300, 3, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::HEALTH_BOOST(), 300, 3, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::SPEED(), 300, 3, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 3, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::INSTANT_HEALTH(), 300, 3, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::STRENGTH(), 300, 3, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::SATURATION(), 300, 3, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::REGENERATION(), 300, 3, false));
+	      $player->getEffects()->add(new EffectInstance(VanillaEffects::WATER_BREATHING(), 300, 3, false));
+	      $player->getEffects()->add(new EffectInstance(VanillaEffects::HASTE(), 300, 3, false));
 	   }
 	   if ($helmet->getStateId() === VanillaBlocks::MOB_HEAD()->setMobHeadType(MobHeadType::DRAGON())->asItem()->getStateId()) {
-	      $player->getEffects()->add(new EffectInstance(VanillaEffects::FIRE_RESISTANCE(), 300, 3, false));
-      	      $player->getEffects()->add(new EffectInstance(VanillaEffects::JUMP_BOOST(), 300, 2, false));
+	      $player->getEffects()->add(new EffectInstance(VanillaEffects::FIRE_RESISTANCE(), 300, 4, false));
+      	      $player->getEffects()->add(new EffectInstance(VanillaEffects::JUMP_BOOST(), 300, 4, false));
               $player->getEffects()->add(new EffectInstance(VanillaEffects::HEALTH_BOOST(), 300, 4, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::SPEED(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::INSTANT_HEALTH(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::STRENGTH(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::SATURATION(), 300, 2, false));
-              $player->getEffects()->add(new EffectInstance(VanillaEffects::REGENERATION(), 300, 2, false));
-	      $player->getEffects()->add(new EffectInstance(VanillaEffects::WATER_BREATHING(), 300, 2, false));
-	      $player->getEffects()->add(new EffectInstance(VanillaEffects::HASTE(), 300, 2, false));
-	      $player->getEffects()->add(new EffectInstance(VanillaEffects::CONDUIT_POWER(), 300, 2, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::SPEED(), 300, 4, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::NIGHT_VISION(), 300, 4, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::INSTANT_HEALTH(), 300, 4, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::STRENGTH(), 300, 4, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::SATURATION(), 300, 4, false));
+              $player->getEffects()->add(new EffectInstance(VanillaEffects::REGENERATION(), 300, 4, false));
+	      $player->getEffects()->add(new EffectInstance(VanillaEffects::WATER_BREATHING(), 300, 4, false));
+	      $player->getEffects()->add(new EffectInstance(VanillaEffects::HASTE(), 300, 4, false));
+	      $player->getEffects()->add(new EffectInstance(VanillaEffects::CONDUIT_POWER(), 300, 4, false));
 	   }
         }
     }
